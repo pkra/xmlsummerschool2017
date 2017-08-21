cdb205e3a8bf238e9d003d693a118f48b45c7d47
Peter Krautzberger <p.krautzberger@gmail.com>
2017-08-21 23:31:13 +0200

    all your base are belong to us

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 fullpage.initialize('#fullpage', {
-		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+		anchors: ['firstPage', 'section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
 		menu: '#menu',
 		css3:true
 	});

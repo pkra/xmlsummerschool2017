3cc1fda1bc3e3ba6b13289f3de2cf0b6a7272c10
Peter Krautzberger <p.krautzberger@gmail.com>
2017-09-12 21:30:08 +0200

    new version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 fullpage.initialize('#fullpage', {
-		anchors: ['firstPage', 'section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+		anchors: ['firstPage', 'section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9'],
 		menu: '#menu',
 		css3:true
 	});
